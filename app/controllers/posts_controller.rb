a32019adaa89fef2cd8b809e7071bc47ae073833
rmdashrfv <rmdashrfv@hey.com>
2022-12-21 20:12:37 -0500

    Websockets connect and messages send between clients

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,12 @@ class PostsController < ApplicationController
   def show
     render json: Post.find_by!(id: params[:id])
   end
+
+  def create
+    post = Post.create!(content: params[:content], user_id: 1)
+    ActionCable.server.broadcast('live_feed', {
+      post: post
+    })
+    render json: post
+  end
 end
